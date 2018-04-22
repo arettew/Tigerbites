9ddea77dca54abf72636d18968f684ca64ab53bd
Abigail Rettew <abigailrettew@nat-oitwireless-inside-vapornet100-c-17357.Princeton.EDU>
2018-04-22 14:16:13 -0400

    added new correct command that currently removes leading commas from earlier version of update.py

---
+++ food/management/commands/correct.py
@@ -0,0 +1,14 @@
+from django.core.management.base import BaseCommand, CommandError
+from food.models import FoodItem 
+
+class Command(BaseCommand):
+    help = 'Correct any mistaks that may have occurred within saving items'
+
+    def handle(self, *args, **options):
+        self.stdout.write("Removing excess commas...")
+        all_items = FoodItem.objects.all()
+        for item in all_items: 
+            if item.dhall != "" and item.dhall[0] == ",":
+                item.dhall = item.dhall[1:]
+                item.save()
+        self.stdout.write("Finished")
