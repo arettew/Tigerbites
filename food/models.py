b3730ce55c994aa1f133afc6a3f36556ac95e9e6
Abigail Rettew <abigailrettew@nat-oitwireless-inside-vapornet100-c-17357.Princeton.EDU>
2018-05-08 18:42:57 -0400

    lengthed dhall field

--- food/models.py
+++ food/models.py
@@ -6,7 +6,7 @@ class FoodItem(models.Model):
    item_name = models.CharField(max_length=200, default="", primary_key=True)
    category = models.CharField(max_length=200, default="")
    meal = models.CharField(max_length=15, default="")
-   dhall = models.CharField(max_length = 100, default = "")
+   dhall = models.CharField(max_length = 200, default = "")
    calories = models.IntegerField(default=0)
    fat = models.FloatField(default=0)
    protein = models.FloatField(default=0)
