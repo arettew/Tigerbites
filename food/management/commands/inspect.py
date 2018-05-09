d1b94dc7babc8d8c8eb2fbeaa2a968d8e1522754
Abigail Rettew <abigailrettew@nat-oitwireless-inside-vapornet100-c-17357.Princeton.EDU>
2018-05-09 11:56:07 -0400

    Added inspect command

---
+++ food/management/commands/inspect.py
@@ -0,0 +1,11 @@
+from django.core.management.base import BaseCommand, CommandError
+from food.models import FoodItem 
+
+class Command(BaseCommand):
+    help = 'Helps to inspect database items'
+
+    def handle(self, *args, **options):
+        items = FoodItem.objects.filter(category__contains = 'Vegetarian')
+        for item in items: 
+            print(item.item_name)
+        self.stdout.write(self.style.SUCCESS("Success"))
