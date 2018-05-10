ea16dcfa3aef5010fa38a4b5bb3b989702c2e350
Abigail Rettew <abigailrettew@nat-oitwireless-inside-vapornet100-c-17357.Princeton.EDU>
2018-05-09 22:00:17 -0400

    added index of daily item to data

--- food/views.py
+++ food/views.py
@@ -7,7 +7,25 @@ import datetime
 
 # Create your views here.
 def today(request):
-    food = scrapeTigerMenus()
+    scraping_results = scrapeTigerMenus()
+    all_items = list(FoodItem.objects.all().values_list('item_name', flat=True))
+    
+    food = {}
+
+    # Get the index number of the item in the database 
+    for dhall in scraping_results: 
+        if not dhall in food: 
+            food[dhall] = {}
+        for category in scraping_results[dhall]:
+            if not category in food[dhall]:
+                food[dhall][category] = {}
+            for item in scraping_results[dhall][category]:
+                # Check if we've stored this item 
+                try:
+                    index = all_items.index(item)
+                    food[dhall][category][item] = index
+                except: 
+                    scraping_results[dhall][category].remove(item)
     return HttpResponse(json.dumps(food))
 
 def all(request):
