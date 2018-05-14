446b7a3a0a4234277d5123e9c6aa5cc756fb1c94
Abigail Rettew <abigailrettew@nat-oitwireless-inside-vapornet100-c-17357.Princeton.EDU>
2018-05-14 15:41:35 -0400

    CSRF Exemption

--- tigerbites/settings.py
+++ tigerbites/settings.py
@@ -46,7 +46,6 @@ MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
