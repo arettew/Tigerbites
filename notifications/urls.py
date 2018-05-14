e927c2a189213814acdf795e39fcab6bcebdb09a
Abigail Rettew <abigailrettew@nat-oitwireless-inside-vapornet100-c-17357.Princeton.EDU>
2018-05-14 15:58:40 -0400

    Remove decorators

--- notifications/urls.py
+++ notifications/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path 
 from . import views 
-from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
-    path('', csrf_exempt(views.token), name='token')
+    path('', views.token, name='token')
 ]
